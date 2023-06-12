b9f93e83680a2c8b0ebf9238996ff4166e12bd4a
hj <hekjun@gmail.com>
2023-06-12 18:52:23 +0800

    new 实现

---
+++ src/javascript/new.js
@@ -0,0 +1,7 @@
+function MyNew() {
+  let obj = new Object({})
+  const constructor = [].slice.call(arguments)
+  obj.__proto__ = constructor.prototype
+  const res = constructor.apply(obj, arguments)
+  return typeof res === 'object' ? res : obj
+}
